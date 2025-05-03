69dcfa55a599d98e50471511c54f8a21da911a24
shamsuttabriz <shamsuttabriz.md@gmail.com>
2025-05-04 01:21:33 +0600

    Page location related some issues are fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Hero Section</h1>
-      <p>All Phone Showcase</p>
+      <h1 className="text-3xl text-center">Hero Section</h1>
+      <p className="text-3xl text-center">All Phone Showcase</p>
     </div>
   );
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router";
-import App from "../App";
 import Favorite from "../Favorite";
 import Home from "../pages/Home";
 import MainLayout from "../layouts/MainLayout";
